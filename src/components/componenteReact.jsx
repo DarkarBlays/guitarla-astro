16bd0eabb0be6a4cc6f33e1b0b8d74fd969a7b3d
DarkarBlays <juanpamendoza08@gmail.com>
2024-02-25 13:12:56 -0500

    completo

---
+++ src/components/componenteReact.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+
+
+export default function ComponenteReact() {
+    const [cliente, setCliente] = useState('Juan')
+    return (
+        <div>{cliente}</div>
+    )
+}
